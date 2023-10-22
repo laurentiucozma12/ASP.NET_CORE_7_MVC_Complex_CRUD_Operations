5452f808e5a023dfc55c42bc82d46c04b7d384a7
Lau <laurentiucozma12@gmail.com>
2023-10-22 16:10:49 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Learn_ASP.NET_CRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Learn_ASP.NET_CRUD.Models
+{
+    public class Product
+    {
+        [Key]
+        [StringLength(6)]
+        public string? Code { get; set; }
+        [Required]
+        [StringLength(75)]
+        public string? Name { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string? Description { get; set; }
+        [Required]
+        [Column(TypeName = "smallmoney")]
+        public decimal Cost {  get; set; }
+    }
+}
